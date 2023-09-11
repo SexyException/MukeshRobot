0099365db8cf4d45fc818613be3f82e145b56ea7
EXCEPTION <142222853+SexyException@users.noreply.github.com>
2023-09-11 14:58:09 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
 TITLE Sanjana Robot
-:: Enables virtual env mode and then starts EXCEPTIOM
+:: Enables virtual env mode and then starts EXCEPTION
 env\scripts\activate.bat && py -m MukeshRobot
